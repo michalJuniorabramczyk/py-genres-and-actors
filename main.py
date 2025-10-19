6a29c3d7a6576fa296debaed84a1a54667c46b24
michalJuniorabramczyk <michaljuniorabramczyk@gmail.com>
2025-10-19 12:58:16 +0100

    Implement Cinema Hall task: add Genre and Actor models, main function with create/update/delete/query

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import init_django_orm  # noqa: F401
-
 from django.db.models import QuerySet
+from db.models import Actor, Genre
+import sys
+import legacy as cgi
+sys.modules["cgi"] = cgi
 
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.get_or_create(name="Western")
+    Genre.objects.get_or_create(name="Action")
+    Genre.objects.get_or_create(name="Dramma")
+
+    Actor.objects.get_or_create(first_name="George", last_name="Klooney")
+    Actor.objects.get_or_create(first_name="Kianu", last_name="Reverse")
+    Actor.objects.get_or_create(first_name="Scarlet", last_name="Kegan")
+    Actor.objects.get_or_create(first_name="Will", last_name="Smith")
+    Actor.objects.get_or_create(first_name="Jaden", last_name="Smith")
+    Actor.objects.get_or_create(first_name="Scarlett", last_name="Johansson")
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    (Actor.objects.filter(first_name="George", last_name="Klooney").
+     update(last_name="Clooney"))
+    (Actor.objects.filter(first_name="Kianu", last_name="Reverse")
+     .update(first_name="Keanu", last_name="Reeves"))
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
